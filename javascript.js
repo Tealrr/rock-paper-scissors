9c96d01d668ba65aacd911b7cd9bfa257c7bbf10
Tyler Rodrigues <tyler.r.rodrigues@gmail.com>
2024-06-28 21:07:42 -0700

    Created a function to ask for the human player's choice

--- javascript.js
+++ javascript.js
@@ -11,3 +11,8 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    const choice = prompt("Time to play Rock Paper Scissors! \
+        What would you like to throw?")
+    return return choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+}
