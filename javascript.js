04d8185d445b6a1ca0ad082e23dde80a9855d62c
Tyler Rodrigues <tyler.r.rodrigues@gmail.com>
2024-06-28 21:01:30 -0700

    Created the function that gets a random choice between rock, paper, and scissors from the computer player

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+function getComputerChoice() {
+    const randNum = Math.floor(Math.random() * 3)
+    if (randNum === 0) {
+        return "Rock"
+    }
+    elif (randNum === 1) {
+        return "Paper"
+    }
+    else {
+        return "Scissors"
+    }
+}
+
