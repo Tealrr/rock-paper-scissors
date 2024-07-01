8e04a741157b4c847abe9248f5c30979c6172095
Tyler Rodrigues <tyler.r.rodrigues@gmail.com>
2024-07-01 16:16:27 -0700

    Cleaned and debugged my code, then added score variables

--- javascript.js
+++ javascript.js
@@ -1,18 +1,20 @@
 function getComputerChoice() {
     const randNum = Math.floor(Math.random() * 3)
     if (randNum === 0) {
-        return "Rock"
+        return "Rock";
     }
-    elif (randNum === 1) {
-        return "Paper"
+    else if (randNum === 1) {
+        return "Paper";
     }
     else {
-        return "Scissors"
+        return "Scissors";
     }
 }
 
 function getHumanChoice() {
-    const choice = prompt("Time to play Rock Paper Scissors! \
-        What would you like to throw?")
+    const choice = prompt("Time to play Rock Paper Scissors! What would you like to throw?")
     return choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
 }
+
+let humanScore = 0
+let computerScore = 0
